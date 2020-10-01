c11c9761919d7bac24506ee58c6b7d70f7e7eacd
Redwan <redwan@integral-service.fr>
2020-10-01 16:00:53 +0200

    add configuration file

---
+++ config/configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+// Accès database
+const BD_HOST   = '';
+const BD_DBNAME = '';
+const BD_USER   = '';
+const BD_PWD    = '';
+
+// Langage of site
+const LANG = 'FR-fr';
+
+// Racine folders
+define('PATH_CONTROLLERS', './controllers/c_');
+define('PATH_ENTITY', './entities/');
+define('PATH_ASSETS', './assets/');
+define('PATH_LIB', './lib/');
+define('PATH_MODELS', './models/');
+define('PATH_VIEWS', './views/v_');
+define('PATH_TEXTES', './languages/');
+
+define('PATH_CSS', PATH_ASSETS.'css/');
+define('PATH_IMG', PATH_ASSETS.'images/');
+define('PATH_JS', PATH_ASSETS.'js/');
+define('PATH_FONT', PATH_ASSETS.'fonts/');
